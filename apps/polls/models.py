8f21a92edeedecfaecb97f3081a85f74d5050d8f
Louis Agyapong <agyapong.louis@gmail.com>
2022-02-03 22:53:54 +0000

    poll models

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self) -> str:
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="choices", verbose_name=_("question")
+    )
+    choice_text = models.CharField(_("Choice text"), max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
+
+    class Meta:
+        verbose_name = _("Choice")
+        verbose_name_plural = _("Choices")
