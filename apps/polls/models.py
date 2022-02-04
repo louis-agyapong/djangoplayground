47b19d3e0a50a02d4994555a22237534f03ffdd0
Louis Agyapong <agyapong.louis@gmail.com>
2022-02-04 07:39:34 +0000

    changed meta options on choice

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
@@ -8,6 +11,9 @@ class Question(models.Model):
 
     def __str__(self) -> str:
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
