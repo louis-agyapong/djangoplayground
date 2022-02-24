ff7d524f841e09bc79b0b2b9d99c7ba53bd67ee5
Louis Agyapong <agyapong.louis@gmail.com>
2022-02-24 12:23:15 +0000

    removed required option on liked field

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, verbose_name=_("author"), on_delete=models.CASCADE)
     title = models.CharField(max_length=200, verbose_name=_("title"), blank=True)
     body = models.TextField(_("body"))
-    liked = models.ManyToManyField(User, verbose_name=_("liked"), related_name="liked_posts")
+    liked = models.ManyToManyField(User, verbose_name=_("liked"), related_name="liked_posts", blank=True)
     created = models.DateTimeField(_("created at"), auto_now_add=True)
 
     def __str__(self) -> str:
