731e550de497b05829963b6ff5fc40e0781085b3
Louis Agyapong <agyapong.louis@gmail.com>
2022-02-24 12:50:57 +0000

    export csv

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -5,9 +5,24 @@ from import_export import resources
 
 
 class PostResource(resources.ModelResource):
+    author = resources.Field()
+    liked = resources.Field()
+    created = resources.Field()
+
     class Meta:
         model = Post
         fields = ("id", "author", "title", "body", "liked", "created")
+
+    def dehydrate_author(self, post) -> str:
+        return str(post.author.username)
+
+    def dehydrate_liked(self, post) -> str:
+        data = [x.username for x in post.liked.all()]
+        users_liked = ", ".join(data)
+        return str(users_liked)
+
+    def dehydrate_created(self, post) -> str:
+        return str(post.created.strftime("%d-%m-%Y"))
 
 
 @admin.register(Post)
