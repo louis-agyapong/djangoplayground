4af5b94da5eba36d9e71a2d4f37425a2b24862eb
Louis Agyapong <agyapong.louis@gmail.com>
2022-02-28 12:36:00 +0000

    blog post templates

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -4,4 +4,4 @@ from .models import Post
 
 def post_list(request) -> render:
     posts = Post.published.all()
-    return render(request, "blog/index.html", {"posts": posts})
+    return render(request, "blog/post_list.html", {"posts": posts})
