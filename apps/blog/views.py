ecba3e4cc854b0dd151cba3eecfe640cb024aa6b
Louis Agyapong <agyapong.louis@gmail.com>
2022-03-02 12:09:29 +0000

    blog post pagination view

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request) -> render:
+    objects_list = Post.published.all()
+    # 3 post in each page
+    paginator = Paginator(objects_list, 3)
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
     posts = Post.published.all()
-    return render(request, "blog/post_list.html", {"posts": posts})
+    return render(request, "blog/post_list.html", {"posts": posts, "page": page})
 
 
 def post_detail(request, year, month, day, post):
