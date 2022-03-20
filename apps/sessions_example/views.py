610eb4defed1d3e1005cdd8617be3b2097a835c2
Louis Agyapong <agyapong.louis@gmail.com>
2022-03-20 01:49:35 +0000

    sessions example

--- apps/sessions_example/views.py
+++ apps/sessions_example/views.py
@@ -3,7 +3,10 @@ from django.template import Template, Context
 
 
 def home_view(request):
-    request.session["visits"] = request.session.get("visits", 0) + 1
+    num_visits = int(request.session.get("num_visits", 0)) + 1
+    request.session["num_visits"] = num_visits
+    # if num_visits > 5:
+    #     del request.session["num_visits"]
     t = Template("<h1>Visits: {{ visits }}</h1>")
-    c = Context({"visits": request.session["visits"]})
+    c = Context({"visits": request.session["num_visits"]})
     return HttpResponse(t.render(c))
