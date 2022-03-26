07adc17f0193825c6a54066d668604d3ce97d623
Louis Agyapong <agyapong.louis@gmail.com>
2022-03-26 08:26:39 +0000

    Event admin

--- apps/event/admin.py
+++ apps/event/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("name", "location", "start_time", "end_time")
+    list_filter = ["start_time"]
+    search_fields = ["name"]
